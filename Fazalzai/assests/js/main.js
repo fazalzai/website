e67ef1fac83b7ae19f1eb5d835e1086118a00f9e
Fazalzai <168582659+fazalzai@users.noreply.github.com>
2024-09-08 08:51:15 -0700

    Add files via upload

---
+++ Fazalzai/assests/js/main.js
@@ -0,0 +1,13 @@
+Aos.init();
+
+
+Aos.init({
+
+    offset: 120,
+    delay: 0,
+    duration: 400,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+});
